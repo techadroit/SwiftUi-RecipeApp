fd04e1aa04db30d56f9e7e73d6d45850c199a12b
dev.thrivent <devasahayam.nadar@thrivent.com>
2023-05-08 11:41:56 +0530

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Nadar, Devasahayam on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
